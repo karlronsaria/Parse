04df410f07eccb4d19aa56e6c7992121b7820418
karlronsaria <andrew.mikedaniels@gmail.com>
2022-10-02 16:06:21 -0700

    refactor(Parse): rename `AParse::Get(self)` to `AParse::Fn(self)`

--- demo.cpp
+++ demo.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include "lib/Parse.h"
+#include "lib/Parsers.h"
 
 class Tests {
 public:
@@ -16,87 +16,6 @@ std::string Tests::WorkingDirectory;
 
 // #define RUN_TESTS
 
-struct Triple {
-    bool success;
-    std::string quotient;
-    std::string remainder;
-
-    bool Success() const {
-        return success;
-    }
-
-    const Triple Copy(bool decision) const {
-        return Triple {
-            .success = decision,
-            .quotient = quotient,
-            .remainder = remainder,
-        };
-    }
-
-    const Triple operator!() const {
-        return Triple {
-            .success = !success,
-            .quotient = quotient,
-            .remainder = remainder,
-        };
-    }
-
-    const Triple Diff(std::function<bool(char)> condition) const {
-        return remainder.length() == 0 || !condition(remainder[0])
-            ? Copy(false)
-            : Triple {
-                .success = true,
-                .quotient = std::string(quotient) + remainder[0],
-                .remainder = remainder.substr(
-                    1,
-                    remainder.length() - 1
-                ),
-            };
-    }
-
-    const Triple By(char divisor) const {
-        return remainder.length() == 0 || remainder[0] != divisor
-            ? Copy(false)
-            : Triple {
-                .success = true,
-                .quotient = std::string(quotient) + divisor,
-                .remainder = remainder.substr(
-                    1,
-                    remainder.length() - 1
-                ),
-            };
-    }
-
-    const Triple By(std::string divisor) const {
-        auto dividend = remainder;
-
-        bool fail = (dividend.length() < divisor.length())
-            || (dividend.substr(0, divisor.length()) != divisor);
-
-        if (fail)
-            return !*this;
-
-        auto remainder = dividend.substr(
-            divisor.length(),
-            dividend.length() - divisor.length()
-        );
-
-        return Triple {
-            .success = true,
-            .quotient = std::string(quotient) + std::string(divisor),
-            .remainder = remainder,
-        };
-    }
-
-    const Triple By(const char * divisor) const {
-        return By(std::string(divisor));
-    }
-
-    const Triple operator/(char divisor) const { return By(divisor); }
-    const Triple operator/(std::string divisor) const { return By(divisor); }
-    const Triple operator/(const char * divisor) const { return By(divisor); }
-};
-
 void PrintTest(std::shared_ptr<AParse<Triple>> p, const std::string & dividend) {
     auto final = p->Get()(
         Triple {
@@ -111,6 +30,27 @@ void PrintTest(std::shared_ptr<AParse<Triple>> p, const std::string & dividend) 
     std::cout
         << "Quotient  : " << final.quotient << '\n'
         << "Remainder : " << final.remainder << "\n\n"
+        ;
+}
+
+void PrintTest(std::shared_ptr<AParse<Integral>> p, const std::string & dividend) {
+    auto final = p->Get()(Integral::New(
+        Triple {
+            .success = true,
+            .quotient = "",
+            .remainder = dividend,
+        },
+        0
+    ));
+
+    if (!final.success)
+        std::cout << "Fail:\n";
+
+    std::cout
+        << "Quotient  : " << final.quotient << '\n'
+        << "Remainder : " << final.remainder << '\n'
+        << "Value     : " << final.value << '\n'
+        << '\n'
         ;
 }
 
@@ -174,10 +114,12 @@ int main(int argc, char ** args) {
         // not not ((c is not '0') and (c is digit))
         // not (not (c is not '0') or not (c is digit))
 
+/*
         auto digit = Parse<Triple>::New((std::function<bool(char)>)isdigit);
-        // auto nonzero_digit = Xor(-Parse<Triple>::New('0'), digit);
-
         auto integer = (Xor(-Parse<Triple>::New('0'), digit) * While(digit)) + digit;
+*/
+
+        auto integer = Parsers::Integer<Integral>();
         PrintTest(integer, "19991xxxx");
         PrintTest(integer, "09991xxxx");
         PrintTest(integer, "00991xxxx");
---
+++ lib/Parsers.h
@@ -0,0 +1,193 @@
+#include "Parse.h"
+
+struct Triple {
+    bool success;
+    std::string quotient;
+    std::string remainder;
+
+    bool Success() const {
+        return success;
+    }
+
+    const Triple Copy(bool decision) const {
+        return Triple {
+            .success = decision,
+            .quotient = quotient,
+            .remainder = remainder,
+        };
+    }
+
+    const Triple operator!() const {
+        return Triple {
+            .success = !success,
+            .quotient = quotient,
+            .remainder = remainder,
+        };
+    }
+
+    const Triple Diff(std::function<bool(char)> condition) const {
+        return remainder.length() == 0 || !condition(remainder[0])
+            ? Copy(false)
+            : Triple {
+                .success = true,
+                .quotient = std::string(quotient) + remainder[0],
+                .remainder = remainder.substr(
+                    1,
+                    remainder.length() - 1
+                ),
+            };
+    }
+
+    const Triple By(char divisor) const {
+        return remainder.length() == 0 || remainder[0] != divisor
+            ? Copy(false)
+            : Triple {
+                .success = true,
+                .quotient = std::string(quotient) + divisor,
+                .remainder = remainder.substr(
+                    1,
+                    remainder.length() - 1
+                ),
+            };
+    }
+
+    const Triple By(const std::string & divisor) const {
+        auto dividend = remainder;
+
+        bool fail = (dividend.length() < divisor.length())
+            || (dividend.substr(0, divisor.length()) != divisor);
+
+        if (fail)
+            return !*this;
+
+        auto remainder = dividend.substr(
+            divisor.length(),
+            dividend.length() - divisor.length()
+        );
+
+        return Triple {
+            .success = true,
+            .quotient = std::string(quotient) + std::string(divisor),
+            .remainder = remainder,
+        };
+    }
+
+    const Triple By(const char * divisor) const {
+        return By(std::string(divisor));
+    }
+
+    const Triple operator/(char divisor) const { return By(divisor); }
+    const Triple operator/(const std::string & divisor) const { return By(divisor); }
+    const Triple operator/(const char * divisor) const { return By(divisor); }
+};
+
+struct Integral: public Triple {
+private:
+    static int Divide(int value, char c) {
+        return (10 * value) + (int)(c - '0');
+    }
+
+    static int Divide(int value, const std::string & str) {
+        for (char c : str)
+            value = Divide(value, c);
+
+        return value;
+    }
+public:
+    int value;
+
+    static Integral New(Triple base, int value) {
+        Integral temp;
+        temp.success = base.success;
+        temp.quotient = base.quotient;
+        temp.remainder = base.remainder;
+        temp.value = value;
+        return temp;
+    }
+
+    const Integral Copy(bool decision) const {
+        Triple t = Triple::Copy(decision);
+        return New(t, value);
+    }
+
+    const Integral Diff(std::function<bool(char)> condition) const {
+        Triple t = Triple::Diff(condition);
+
+        int i = t.success
+            ? Divide(value, t.quotient[t.quotient.length() - 1])
+            : value
+            ;
+
+        return New(t, i);
+    }
+
+    const Integral operator!() const {
+        Triple t = Triple::operator!();
+        return New(t, value);
+    }
+
+    const Integral operator/(char divisor) const {
+        Triple t = Triple::operator/(divisor);
+
+        int i = t.success
+            ? Divide(value, divisor)
+            : value
+            ;
+
+        return New(t, i);
+    }
+
+    const Integral operator/(const std::string & divisor) const {
+        Triple t = Triple::operator/(divisor);
+
+        int i = t.success
+            ? Divide(value, divisor)
+            : value
+            ;
+
+        return New(t, i);
+    }
+
+    const Integral operator/(const char * divisor) const {
+        return *this / std::string(divisor);
+    }
+};
+
+namespace Parsers {
+    template <__DIVIDEND__ T>
+    std::shared_ptr<AParse<T>>
+    Digit() {
+        return Parse<T>::New((std::function<bool(char)>)isdigit);
+    }
+
+    template <__DIVIDEND__ T>
+    std::shared_ptr<AParse<T>>
+    Alpha() {
+        return Parse<T>::New((std::function<bool(char)>)isalpha);
+    }
+
+    template <__DIVIDEND__ T>
+    std::shared_ptr<AParse<T>>
+    Space() {
+        return Parse<T>::New((std::function<bool(char)>)isspace);
+    }
+
+    template <__DIVIDEND__ T>
+    std::shared_ptr<AParse<T>>
+    Punct() {
+        return Parse<T>::New((std::function<bool(char)>)ispunct);
+    }
+
+    template <__DIVIDEND__ T>
+    std::shared_ptr<AParse<T>>
+    Lower() {
+        return Parse<T>::New((std::function<bool(char)>)islower);
+    }
+
+    template <__DIVIDEND__ T>
+    std::shared_ptr<AParse<T>>
+    Integer() {
+        auto digit = Digit<T>();
+        return (Xor(-Parse<T>::New('0'), digit) * While(digit)) + digit;
+    }
+};
